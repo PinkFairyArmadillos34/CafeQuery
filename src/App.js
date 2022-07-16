fe5e674859ec85ae3eedf58106a2f5c48ef018ce
Taryn Cunha <taryn.cunha@gmail.com>
2022-07-16 11:48:28 -0400

    Add react router functionality to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import { BroswerRouter as Router, Switch, Route, Link } from "react-router-dom";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AddSpaceReview from './components/AddSpaceReview';
+import DisplaySpaces from './components/DisplaySpaces';
+import HomePage from './components/HomePage';
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
+function App()  {
     return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-        <button type="button" class="btn btn-primary">
-          This is a bootstrap button
-        </button>
-      </>
+     <Router>
+      <nav className = "navbar">
+        <div className="navbar-nav">
+        <li className="nav-item>">
+            <Link to={"/"} className ="nav-link">
+              Home Page
+            </Link>
+          </li>
+          <li className="nav-item>">
+            <Link to={"/display"} className ="nav-link">
+              Locations
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add a location
+            </Link>
+          </li>
+        </div>
+      </nav>
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/display" component={DisplaySpaces} />
+          <Route exact path="/add" component={AddSpaceReview} />
+        </Switch>
+      </div>
+     </Router>
     );
-  }
 }
+
 
 export default hot(App);
