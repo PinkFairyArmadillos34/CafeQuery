c3a74f19c2a2c42c09393a703743661db0382df5
nikolajsvv <nikolajsvv@gmail.com>
2022-07-16 10:07:53 -0400

    added server folder structure/created basic server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');;
+
+const app = express();
+
+const PORT = 3000;
+
+// const mongoURI = "mongodb+srv://codesmith:cs@cluster0.di70nhs.mongodb.net/?retryWrites=true&w=majority";
+
+// need to determine how we are parsing data
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occured'}
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+})
