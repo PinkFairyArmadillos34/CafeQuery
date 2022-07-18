7387b3136b2f0eb549f261f2d6468862b71d0efb
Taryn Cunha <taryn.cunha@gmail.com>
2022-07-18 08:58:49 -0400

    Add axios post to add workspace in actions js

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,13 +1,20 @@
 
 // import actionType constants by type
+import axios from 'axios';
 import * as types from '../constants/actionTypes';
 
 
 //functions to be used in dispatch
-export const addWorkSpace = workSpaceInfo => ({
-  type: types.ADD_SPACE,
-  payload: workSpaceInfo
-});
+export const addWorkSpace = workSpaceInfo => dispatch => {
+  axios.post(`localhost:3000/workspace`)
+  .then((workSpaceInfo) => {
+    dispatch({
+      type: types.ADD_SPACE,
+      payload: workSpaceInfo})
+  })
+  .catch(err => {console.log(err)});
+};
+
 
 export const updateWorkSpace = workSpaceInfo => ({
   type: types.UPDATE_SPACE,
