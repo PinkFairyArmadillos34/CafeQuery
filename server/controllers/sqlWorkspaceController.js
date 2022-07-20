0d2c151adc86df1e4a5b369641af133fe5514264
fried-jo <101134988+fried-jo@users.noreply.github.com>
2022-07-20 10:29:41 -0400

    Started working on SQL routes

--- server/controllers/sqlWorkspaceController.js
+++ server/controllers/sqlWorkspaceController.js
@@ -4,7 +4,6 @@ const sqlDB = require('../models/sqlModels.js');
 const sqlWorkspaceController = {};
 
 sqlWorkspaceController.createWorkspace = async function (req, res, next) {
-  // console.log('UP HERE BITCH');
   const {
     workspaceName,
     zipcode,
@@ -27,9 +26,7 @@ sqlWorkspaceController.createWorkspace = async function (req, res, next) {
   } = req.body;
   const queryString = `
     INSERT INTO workspaces(WorkspaceName, Zipcode, Address, Rating, Wifi, Type, Quiet, Outlets, TimeLimit, LaptopRestrictions, Crowded, OutdoorSeating, PetFriendly, URL, FoodRating, CoffeeRating, Seating, Other)
-    VALUES('${workspaceName}', '${zipcode}', '${address}', '${rating}', '${wifi}', '${type}', '${quiet}', '${outlets}', '${timeLimit}', '${laptopRestrictions}', '${crowded}', '${outdoorSeating}', '${petFriendly}', '${url}', '${foodRating}', '${coffeeRating}', '${seating}', '${other}')`;
-  // console.log('Im here');
-  const testQuery = `INSERT INTO test(testName, octopusName) VALUES ('${req.body.testName}', '${req.body.octopusName}') RETURNING *`;
+    VALUES('${workspaceName}', '${zipcode}', '${address}', '${rating}', '${wifi}', '${type}', '${quiet}', '${outlets}', '${timeLimit}', '${laptopRestrictions}', '${crowded}', '${outdoorSeating}', '${petFriendly}', '${url}', '${foodRating}', '${coffeeRating}', '${seating}', '${other}') RETURNING *`;
   try {
     const result = await sqlDB.query(queryString);
     res.locals.newWorkspace = result.rows;
